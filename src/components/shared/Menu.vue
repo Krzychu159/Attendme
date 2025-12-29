bf820fe9e0232425b79ea5e45c3cbbf2add7e9c9
Krzychu159 <krzysztof.rak159@gmail.com>
2025-12-29 19:02:16 +0100

    popups in teachercourseview

--- src/components/shared/Menu.vue
+++ src/components/shared/Menu.vue
@@ -12,9 +12,9 @@
         </li>
       </RouterLink>
 
-      <div ref="dropdownRef" class="relative">
+      <div class="relative">
         <li
-          @click="toggle"
+          @click="open = !open"
           class="bg-blue-500 p-4 rounded-full text-white font-bold cursor-pointer select-none"
         >
           {{ name.slice(0, 1) }}{{ surname.slice(0, 1) }}
@@ -22,7 +22,7 @@
 
         <div
           v-if="open"
-          class="absolute right-0 mt-2 w-56 bg-white shadow-lg rounded-lg z-50"
+          class="absolute right-0 top-full mt-2 w-56 bg-white shadow-lg rounded-lg z-50"
         >
           <div class="p-4 text-sm">
             <div class="font-semibold">{{ name }} {{ surname }}</div>
@@ -38,10 +38,13 @@
         </div>
       </div>
     </nav>
+
+    <div v-if="open" class="fixed inset-0 z-40" @click="open = false"></div>
   </div>
 </template>
+
 <script setup lang="ts">
-import { ref, onMounted, onBeforeUnmount } from "vue";
+import { ref } from "vue";
 import { useAuthStore } from "../../store/auth";
 
 const auth = useAuthStore();
@@ -51,23 +54,4 @@ const surname = auth.user?.surname || "";
 const role = auth.role || "guest";
 
 const open = ref(false);
-const dropdownRef = ref<HTMLElement | null>(null);
-
-const toggle = () => {
-  open.value = !open.value;
-};
-
-const handleClickOutside = (e: MouseEvent) => {
-  if (dropdownRef.value && !dropdownRef.value.contains(e.target as Node)) {
-    open.value = false;
-  }
-};
-
-onMounted(() => {
-  document.addEventListener("click", handleClickOutside);
-});
-
-onBeforeUnmount(() => {
-  document.removeEventListener("click", handleClickOutside);
-});
 </script>
--- src/components/teacher/TeacherCourseView.vue
+++ src/components/teacher/TeacherCourseView.vue
@@ -22,10 +22,106 @@
         </div>
       </div>
       <div class="flex flex-col gap-5 mb-8 md:mb-0">
-        <button class="bg-blue-500 border-blue-500 text-white">
+        <button
+          class="bg-blue-500 border-blue-500 text-white"
+          @click="openScanner = !openScanner"
+        >
           Skaner obecności
         </button>
-        <button>Rejestracja urządzenia</button>
+        <button @click="openRegister = !openRegister">
+          Rejestracja urządzenia
+        </button>
+      </div>
+    </div>
+
+    <!-- OVERLAY -->
+    <div
+      v-if="openRegister || openScanner"
+      class="fixed inset-0 z-40 bg-black/40"
+      @click.self="
+        openRegister = false;
+        openScanner = false;
+      "
+    >
+      <!-- REGISTER -->
+      <div
+        v-if="openRegister"
+        class="absolute top-8 left-1/2 -translate-x-1/2 bg-white border p-4 rounded shadow-lg"
+      >
+        <div class="flex justify-between items-center border-b pb-2 mb-5">
+          <div class="font-bold">Rejestracja urządzenia</div>
+          <div
+            class="cursor-pointer text-2xl"
+            @click.self="
+              openRegister = false;
+              openScanner = false;
+            "
+          >
+            &times;
+          </div>
+        </div>
+
+        <table class="w-full border text-sm">
+          <thead class="bg-gray-100">
+            <tr>
+              <th class="p-2 text-left">Student</th>
+              <th class="p-2 text-left">Album</th>
+              <th class="p-2 text-left">Urządzenie</th>
+              <th class="p-2 text-left">Link Rejestracyjny</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr
+              v-for="s in students"
+              :key="s.attenderUserId"
+              class="border-t even:bg-gray-50 odd:bg-white"
+            >
+              <td class="p-2">{{ s.userName }} {{ s.userSurname }}</td>
+              <td class="p-2">
+                {{ s.studentAlbumIdNumber }}
+              </td>
+              <td class="p-2">
+                <div>brak</div>
+              </td>
+              <td class="p-2">
+                <button class="bg-blue-500 border-blue-500 text-white">
+                  Skopiuj Link
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <!-- SCANNER -->
+      <div
+        v-if="openScanner"
+        class="absolute top-40 left-1/2 -translate-x-1/2 bg-white border p-4 rounded shadow-lg flex flex-col gap-4"
+      >
+        <div class="flex justify-between items-center border-b pb-2">
+          <div class="font-bold">Skaner obescności</div>
+          <div
+            class="cursor-pointer text-2xl"
+            @click.self="
+              openRegister = false;
+              openScanner = false;
+            "
+          >
+            &times;
+          </div>
+        </div>
+        <div>
+          Do sprawdzania obecności wymagane jest urządzenie wyposażone w kamerę
+          (tablet lub telefon). Zeskanuj na nim poniższy kod QR lub otwórz adres
+          url, który możesz skopiować poniższym przyciskiem. Sprawdzenie
+          obecności polega na umieszczeniu w polu widzenia kamery skanera kodu
+          QR wygnerowanego na ekranie telefonu uczestnika.
+        </div>
+        <div>Kod QR</div>
+        <button class="bg-blue-500 border-blue-500 text-white">
+          Skopiuj Link
+        </button>
       </div>
     </div>
 
@@ -42,7 +138,11 @@
       </thead>
 
       <tbody>
-        <tr v-for="s in students" :key="s.attenderUserId" class="border-t">
+        <tr
+          v-for="s in students"
+          :key="s.attenderUserId"
+          class="border-t even:bg-gray-50 odd:bg-white"
+        >
           <td class="p-2">{{ s.userName }} {{ s.userSurname }}</td>
           <td class="p-2">
             {{ s.studentAlbumIdNumber }}
@@ -104,6 +204,9 @@ const { session, students, loading, error } = storeToRefs(store);
 
 const teacherCoursesStore = useTeacherCoursesStore();
 const didPrefetch = ref(false);
+
+const openRegister = ref(false);
+const openScanner = ref(false);
 
 const toogleAttendance = (
   studentId: number,
